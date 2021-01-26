532c71050e62cbf2802f3baba23b17a798d2a2cd
celeron1ghz <celeron1ghz@gmail.com>
2021-01-26 23:32:48 +0900

    minimum express implement

--- acceptessa2-administration-login/handler.js
+++ acceptessa2-administration-login/handler.js
@@ -1,14 +1,5 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-      },
-      null,
-      2
-    ),
-  };
-};
+const handler = require('serverless-express/handler')
+const app = require('./app')
+exports.hello = handler(app)
