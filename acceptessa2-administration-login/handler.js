e714709255b4ada6da31dae7e92e38fa32bd0dd8
celeron1ghz <celeron1ghz@gmail.com>
2021-01-27 20:39:31 +0900

    discard express impl

--- acceptessa2-administration-login/handler.js
+++ acceptessa2-administration-login/handler.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const handler = require('serverless-express/handler')
-const app = require('./app')
-exports.hello = handler(app)
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+      },
+      null,
+      2
+    ),
+  };
+};
