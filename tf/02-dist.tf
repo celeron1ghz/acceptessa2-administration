d1d68110b20f71f7ca382c57c59694172a7387c0
celeron1ghz <celeron1ghz@gmail.com>
2021-02-01 22:13:49 +0900

    add error handling to 403, 404

--- tf/02-dist.tf
+++ tf/02-dist.tf
@@ -122,11 +122,17 @@ resource "aws_cloudfront_distribution" "dist" {
     ssl_support_method  = "sni-only"
   }
 
-  # custom_error_response {
-  #   error_code         = "403"
-  #   response_code      = "200"
-  #   response_page_path = "/index.html"
-  # }
+  custom_error_response {
+    error_code         = "403"
+    response_code      = "403"
+    response_page_path = "/403.html"
+  }
+
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "404"
+    response_page_path = "/404.html"
+  }
 }
 
 resource "aws_cloudfront_public_key" "key" {
